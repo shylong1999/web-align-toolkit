6c80122c4628e34b335e24062dd21f3babc9e0e5
Long <43140498+shylong1999@users.noreply.github.com>
2020-12-21 13:32:12 +0700

    change datatable

--- database/2020-12-13/public.sql
+++ database/2020-12-13/public.sql
@@ -11,11 +11,45 @@
  Target Server Version : 100406
  File Encoding         : 65001
 
- Date: 13/12/2020 23:15:19
+ Date: 21/12/2020 13:27:32
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for data_align
+-- ----------------------------
+DROP TABLE IF EXISTS `data_align`;
+CREATE TABLE `data_align`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lang1` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lang2` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `scope` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `user_id` int NOT NULL,
+  `created_date` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
+  `update_date` timestamp(0) NULL DEFAULT NULL,
+  `delete_date` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2562 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sen_align_data
+-- ----------------------------
+DROP TABLE IF EXISTS `sen_align_data`;
+CREATE TABLE `sen_align_data`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lang1` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `lang2` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `scope` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `isCheck` int NOT NULL DEFAULT 0,
+  `isDelete` int NOT NULL DEFAULT 0,
+  `user_id` int NOT NULL,
+  `create_date` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
+  `update_time` timestamp(0) NULL DEFAULT NULL,
+  `delete_date` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2596 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for users
@@ -32,10 +66,5 @@ CREATE TABLE `users`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES (1, 'admin', 'admin', '2020-12-13 22:06:54', 1, 'admin@admin.com', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
